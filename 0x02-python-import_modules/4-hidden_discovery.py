9c4c750b29c5fdcee90f53357695cb004c6b2674
SirTonsam <samutongai@gmail.com>
2023-05-12 14:59:05 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,14 @@
-#!/usr/bin/python3
-import hidden_4
+#!/usr/bin/python3.8
+import dis
 
-if __name__ == '__main__':
-    for name in dir(hidden_4):
-        if not name.startswith('__'):
-            print(name)
+if __name__ == "__main__":
+    module = __import__("hidden_4")
+    for name in sorted(dir(module)):
+        if name[0] != "_" and name[1] != "_":
+            code = getattr(module, name)
+            if isinstance(code, bytes):
+                code = code.decode("utf-8")
+                print(dis.dis(code))
+            else:
+                print(name)
+
