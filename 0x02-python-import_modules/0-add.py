45ed3a1eb78d04721ca23003ad650abe32db1bdf
SirTonsam <samutongai@gmail.com>
2023-05-12 02:30:54 -0700

    add_0.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,14 @@
-#!/usr/bin/python3
-a = 1
-b = 2
-
+# Import the add function from the add_0.py file
 from add_0 import add
 
-result = add(a, b)
-print(f"{a} + {b} = {result}")
+# Assign the value 1 to a variable called a
+a = 1
 
+# Assign the value 2 to a variable called b
+b = 2
+
+# Call the add function with the values of a and b as arguments
+result = add(a, b)
+
+# Print the result of the addition
+print(f"{a} + {b} = {result}")
