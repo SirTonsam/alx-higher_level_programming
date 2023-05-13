fe92dcdf1e6469f2ef4dd177128c227397b784ae
SirTonsam <samutongai@gmail.com>
2023-05-13 04:24:18 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 
-
-# Import the add function from the add_0.py file
-from py_compile import main
 from add_0 import add
-
-# Assign the value 1 to a variable called a
-a = 1
-
-# Assign the value 2 to a variable called b
-b = 2
-
-# Call the add function with the values of a and b as arguments
-result = add(a, b)
-
-# Print the result of the addition
-print(f"{a} + {b} = {result}")
+def main():
+    a=1
+    b=2
+    result = add(a,b)
+    print ("{} + {} = {}".format(a,b,result))
 
 if __name__ == "__main__":
     main()
-
