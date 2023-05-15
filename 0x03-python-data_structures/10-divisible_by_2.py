1e2c595891f9e66b0db5b9c20203eb86712f9498
SirTonsam <samutongai@gmail.com>
2023-05-15 11:25:14 -0700

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
+#!/usr/bin/python3
 my_list = [0, 1, 2, 3, 4, 5, 6]
 list_result = divisible_by_2(my_list)
 
 for i in range(len(list_result)):
     print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-
