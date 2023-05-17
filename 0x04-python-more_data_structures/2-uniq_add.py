0c1d7f8a40e7f5fcd34de09f6ea79ac9aa978e14
SirTonsam <samutongai@gmail.com>
2023-05-17 06:32:38 -0700

    3-common_elements.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    """
+    Adds all unique integers in a list (only once for each integer).
 
-def uniq_add(my_list):
-    unique_list = list(set(my_list))
-    result = sum(unique_list)
-    return result
+    Args:
+    my_list (list): A list of integers.
+
+    Returns:
+    int: The sum of all unique integers in the list.
+    """
+
+    unique_integers = set(my_list)
+    sum_of_unique_integers = sum(unique_integers)
 
 
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
+return sum_of_unique_integers
