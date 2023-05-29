5132b128b3e6d71ec6cc1fcee217e3d76c462c12
SirTonsam <samutongai@gmail.com>
2023-05-29 16:05:36 -0700

    101update

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,13 +3,10 @@
 import sys
 
 
-def safe_print_list_integers(lst=[], num=0):
-    count = 0
-    for i in range(0, num):
-        try:
-            print("{:d}".format(lst[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
-            continue
-    print("")
-    return count
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        return None
